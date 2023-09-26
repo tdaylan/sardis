810787b2f02555d0a30c63249e6f725f4a25b39f
Tansu Daylan <tansu.daylan@gmail.com>
2023-09-26 18:43:01 -0500

    NA

--- sardis/main.py
+++ sardis/main.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 
-from astroquery.mast import Catalogs
-from scipy.stats import binned_statistic
-from tessDiffImage import draw_pix_catalog
 
 def init():
     
